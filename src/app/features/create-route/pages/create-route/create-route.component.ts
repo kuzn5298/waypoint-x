ad6f28cb08d3299530865ef85fe63156e4144193
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-03-05 22:56:33 +0100

    👨‍🔧 Small fixes

--- src/app/features/create-route/pages/create-route/create-route.component.ts
+++ src/app/features/create-route/pages/create-route/create-route.component.ts
@@ -1,14 +1,15 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import {
   MapComponent as MaplibreComponent,
   ControlComponent,
   NavigationControlDirective,
 } from '@maplibre/ngx-maplibre-gl';
-import { Map } from 'maplibre-gl';
 import { MapService } from '@/app/core/services/map.service';
 import { GeolocationService } from '@/app/core/services/geolocation.service';
 import { PoiTransportComponent } from '../../../map/layers/poi-transport/poi-transport.component';
 import { PoiComponent } from '../../../map/layers/poi/poi.component';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { map, startWith, switchMap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-create-route',
@@ -22,22 +23,33 @@ import { PoiComponent } from '../../../map/layers/poi/poi.component';
   templateUrl: './create-route.component.html',
   styleUrl: './create-route.component.css',
 })
-export class CreateRouteComponent {
+export class CreateRouteComponent implements OnInit {
   mapService = inject(MapService);
   geolocationService = inject(GeolocationService);
 
   style = computed(() => this.mapService.mapStyle());
+  styleLoaded = toSignal(
+    toObservable(this.style).pipe(
+      switchMap(() =>
+        timer(50).pipe(
+          map(() => true),
+          startWith(false)
+        )
+      )
+    )
+  );
 
-  map = signal<Map>({} as Map);
+  mapCenter = signal<[number, number]>([0, 0]);
+  mapConfigLoaded = signal(false);
 
-  async loadMapStartLocation() {
-    const location = await this.geolocationService.getMyLocation();
-    const center: [number, number] = [location.longitude, location.latitude];
-    this.map().flyTo({ center, zoom: 10, speed: 1.5 });
+  ngOnInit() {
+    this.loadMapConfig();
   }
 
-  mapLoad(map: Map) {
-    this.map.set(map);
-    this.loadMapStartLocation();
+  async loadMapConfig() {
+    const location = await this.geolocationService.getMyLocation();
+    const center: [number, number] = [location.longitude, location.latitude];
+    this.mapCenter.set(center);
+    this.mapConfigLoaded.set(true);
   }
 }
