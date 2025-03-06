fa091e59e743dbac68372f7f7eb5b0122ca47a93
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-03-06 01:26:41 +0100

    👨‍🔧 Rework hover effect and pwa icons

--- src/app/features/map/components/poi-hover-extension/poi-hover-extension.component.ts
+++ src/app/features/map/components/poi-hover-extension/poi-hover-extension.component.ts
@@ -1,4 +1,5 @@
 import { Component, output } from '@angular/core';
+import { Map } from 'maplibre-gl';
 import { MapMouseEvent } from '../../types/MapMouseEvent';
 
 @Component({
@@ -9,11 +10,8 @@ export class PoiHoverExtensionComponent {
 
   hoveredFeatureId: string | number | null = null;
 
-  onMouseMove(event: MapMouseEvent) {
-    const id = event.features?.[0]?.id;
-    if (!id) return;
+  private addHoverEffect(map: Map, id: string | number) {
     this.hoveredFeatureId = id;
-    const map = event.target;
     map.getCanvas().style.cursor = 'pointer';
     map.setFeatureState(
       { source: 'openmaptiles', sourceLayer: 'poi', id },
@@ -21,15 +19,28 @@ export class PoiHoverExtensionComponent {
     );
   }
 
-  onMouseLeave(event: MapMouseEvent) {
+  private removeHoverEffect(map: Map) {
     if (!this.hoveredFeatureId) return;
-    const map = event.target;
     map.getCanvas().style.cursor = '';
     map.setFeatureState(
       { source: 'openmaptiles', sourceLayer: 'poi', id: this.hoveredFeatureId },
       { hover: false }
     );
     this.hoveredFeatureId = null;
+  }
+
+  onMouseMove(event: MapMouseEvent) {
+    const id = event.features?.[0]?.id;
+    if (!id) return;
+    const map = event.target;
+    this.removeHoverEffect(map);
+    this.addHoverEffect(map, id);
+  }
+
+  onMouseLeave(event: MapMouseEvent) {
+    if (!this.hoveredFeatureId) return;
+    const map = event.target;
+    this.removeHoverEffect(map);
   }
 
   onMouseClick(event: MapMouseEvent) {
